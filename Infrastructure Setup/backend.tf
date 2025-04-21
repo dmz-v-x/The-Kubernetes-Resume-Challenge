ae69827ec91f3b703aad084178c9a2db871f0552
Himanshu Bhatt <hbhatt034@gmail.com>
2025-04-21 14:11:48 +0530

    Modified terraform.yaml file

--- Infrastructure Setup/backend.tf
+++ Infrastructure Setup/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "eks-terraform-kubernetes-resume-challenge"
-    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "value"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-kubernetes-resume-challenge"
     encrypt        = true
   }
 }
