88931eeb923cf4ce2b57f30efb9707dd7dce08e2
Himanshu Bhatt <hbhatt034@gmail.com>
2025-04-21 12:45:09 +0530

    Modified region in backend.tf

--- Infrastructure Setup/backend.tf
+++ Infrastructure Setup/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "eks-terraform-kubernetes-resume-challenge"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "terraform-state-lock-kubernetes-resume-challenge"
     encrypt        = true
