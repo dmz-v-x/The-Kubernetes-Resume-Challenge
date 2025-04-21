a693b71f34787b1bbec84142cba9c80214e28f5a
Himanshu Bhatt <hbhatt034@gmail.com>
2025-04-21 14:04:52 +0530

    modified backend.tf

--- Infrastructure Setup/backend.tf
+++ Infrastructure Setup/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "eks-terraform-kubernetes-resume-challenge"
-#     region         = "us-east-1"
-#     key            = "eks/terraform.tfstate"
-#     dynamodb_table = "value"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "eks-terraform-kubernetes-resume-challenge"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "value"
+    encrypt        = true
+  }
+}
